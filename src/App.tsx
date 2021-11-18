1d032356f94cfff3e99bed8ef8441f478764f4cb
SerBEEan <sergey.atwork7@gmail.com>
2021-11-18 19:18:36 +0300

    добавил компонент на страницу

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Sparkline } from './components/Sparkline';
 
-function App() {
+const categories: number[] = [
+  1627648088000,
+  1627648388000,
+  1627648688000,
+  1627648988000,
+  1627649288000,
+  1627649588000,
+  1627649888000,
+  1627650188000,
+  1627650488000,
+  1627650788000,
+  1627651088000,
+  1627651388000,
+];
+const data: number[] = [695396, 867215, 820072, 690322, 581228, 812462, 854535, 521250, 815516, 851173, 862075, 825997];
+
+const a = 5;
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Sparkline name='Ошибки' categories={categories} data={data} width={220} height={84} />
+    </>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { SparklineProvider } from './components/SparklineProvider';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SparklineProvider>
+      <App />
+    </SparklineProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
